7a0b70aa69a3660df1c014145e096c457747dcdc
jlgeli <jlgeli@techfactors.com>
2022-02-06 21:07:34 +0800

    Get users test

---
+++ CleanArchitecture.Test/Application/UserAccounts/Queries/GetUsersTests.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchitecture.Application.UserAccounts.Queries.GetUsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Test.Application.UserAccounts.Queries
+{
+    public class GetUsersTests : BaseTest
+    {
+        private readonly GetUsersQueryHandler _sutHandler;
+
+        public GetUsersTests()
+        {
+            _sutHandler = new GetUsersQueryHandler(_context);
+        }
+
+        [Fact]
+        public async void GetUsers_ShouldGetAllUsers()
+        {
+            // Arrange
+            var query = new GetUsersQuery();
+
+            // Act
+            var response = await _sutHandler.Handle(query, CancellationToken.None);
+
+            // Assert
+            response.Data.Count()
+                .Should().Be(5);
+        }
+    }
+}
