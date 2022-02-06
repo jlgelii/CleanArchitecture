b4f4400f0535fdb2c92ca421bbdc4318a608b66e
jlgeli <jlgeli@techfactors.com>
2022-02-06 09:58:15 +0800

    Add project files.

---
+++ CleanArchitecture.API/Startup.cs
@@ -0,0 +1,68 @@
+using CleanArchitecture.API.Configurations.Extensions;
+using CleanArchitecture.API.Configurations.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using CleanArchitecture.Application;
+using CleanArchitecture.Application.Configurations.Validation;
+
+namespace CleanArchitecture.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddJwt();
+            services.AddSwagger();
+            services.AddValidators();
+
+            services.AddHttpContextAccessor();
+            services.AddMediatR(typeof(EntryPoint).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.ConfigureSwaggerHandler();
+            app.ConfigureExceptionHandler();
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
