c356aeb46b0260841be1abedceaf4ce928e0c15f
Jeffrey Geli <jlgeli@techfactors.com>
2022-07-30 10:47:23 +0800

    Delete person

---
+++ CleanArchitecture.Application/Persons/Command/DeletePerson/DeletePersonCommand.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Persons.Command.DeletePerson
+{
+    public class DeletePersonCommand : IRequest<Response<Person>>
+    {
+        public DeletePersonCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+    }
+}
